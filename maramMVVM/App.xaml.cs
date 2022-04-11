ee5b5558dd307bdb7bbc7dd7d9f3774c8f61161f
Doaa-Eid <78642805+doaa1234@users.noreply.github.com>
2022-04-11 17:04:40 +0300

    Add files via upload

---
+++ maramMVVM/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using maramMVVM.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace maramMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            //  MainPage = new NavigationPage(new TempletPageView());
+            MainPage=new TempletPageView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ maramMVVM/Model/TempletPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maramMVVM.Model
+{
+    public class TempletPageModel
+    {
+
+        public string text { get; set; }    
+    }
+}
